1a1c3cc74b9d2d8c7a762fd16cea52cbedec9841
Alan Ambrose <alanambrosedev@gmail.com>
2026-03-13 22:54:29 +0530

    feat: add Bootstrap and custom styles, update Home component with Navbar
    - Added Bootstrap and React-Bootstrap dependencies to package.json
    - Created a new SCSS file for custom styles
    - Updated Home component to include a responsive Navbar with branding and navigation links
    - Imported Bootstrap CSS and custom styles in main.jsx

--- src/components/common/Home.jsx
+++ src/components/common/Home.jsx
@@ -1,10 +1,35 @@
 import React from "react";
-
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import Logo from "../../assets/images/logo.png";
 const Home = () => {
   return (
-    <div>
-      <h1>Home Page</h1>
-    </div>
+    <>
+      <header className="shadow">
+        <div className="bg-dark text-center py-3">
+          <span className="text-white">Your Fashion Partner</span>
+        </div>
+        <div className="container">
+          <Navbar expand="lg" className="">
+            <Navbar.Brand href="#">
+              <img src={Logo} alt="" width={170} />
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="navbarScroll" />
+            <Navbar.Collapse id="navbarScroll">
+              <Nav
+                className="ms-auto my-2 my-lg-0"
+                style={{ maxHeight: "100px" }}
+                navbarScroll
+              >
+                <Nav.Link href="#action1">Mens</Nav.Link>
+                <Nav.Link href="#action2">Women</Nav.Link>
+                <Nav.Link href="#action2">Kids</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+        </div>
+      </header>
+    </>
   );
 };
 
