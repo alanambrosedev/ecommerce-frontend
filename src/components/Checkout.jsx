bd8d4b9016c8ba50a8578d460dfb3ae9d354461c
Alan Ambrose <alanambrosedev@gmail.com>
2026-03-30 22:18:20 +0530

    feat: enhance Checkout component with billing details form and breadcrumb navigation

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Layout from "./common/Layout";
+import { Link } from "react-router-dom";
 
 const Checkout = () => {
   return (
     <Layout>
-      <div className="container">
+      <div className="container pb-5">
         <div className="row">
           <div className="col-md-12">
             <nav aria-label="breadcrumb" className="py-4">
@@ -12,14 +13,85 @@ const Checkout = () => {
                 <li class="breadcrumb-item">
                   <Link to="/">Home</Link>
                 </li>
-                <li class="breadcrumb-item" aria-current="page">
-                  <Link to="/shop">Shop</Link>
-                </li>
                 <li class="breadcrumb-item active" aria-current="page">
-                  <Link to="">Dummy Product</Link>
+                  <Link to="">Checkout</Link>
                 </li>
               </ol>
             </nav>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-7">
+            <h3 className="border-bottom pb-3">
+              <strong>Billing Details</strong>
+            </h3>
+            <form action="">
+              <div className="row pt-3">
+                <div className="col-md-6">
+                  <div>
+                    <input
+                      type="text"
+                      placeholder="Name"
+                      className="form-control"
+                    />
+                  </div>
+                </div>
+                <div className="col-md-6">
+                  <div className="mb-3">
+                    <input
+                      type="text"
+                      placeholder="Email"
+                      className="form-control"
+                    />
+                  </div>
+                </div>
+                <div className="mb-3">
+                  <textarea
+                    type="textarea"
+                    className="form-control"
+                    rows={3}
+                    placeholder="Address"
+                    id=""
+                  />
+                </div>
+                <div className="col-md-6">
+                  <div>
+                    <input
+                      type="text"
+                      placeholder="City"
+                      className="form-control"
+                    />
+                  </div>
+                </div>
+                <div className="col-md-6">
+                  <div className="mb-3">
+                    <input
+                      type="text"
+                      placeholder="State"
+                      className="form-control"
+                    />
+                  </div>
+                </div>
+                <div className="col-md-6">
+                  <div>
+                    <input
+                      type="text"
+                      placeholder="Zip"
+                      className="form-control"
+                    />
+                  </div>
+                </div>
+                <div className="col-md-6">
+                  <div className="mb-3">
+                    <input
+                      type="text"
+                      placeholder="Mobile"
+                      className="form-control"
+                    />
+                  </div>
+                </div>
+              </div>
+            </form>
           </div>
         </div>
       </div>
