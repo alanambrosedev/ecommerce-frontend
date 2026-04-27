3a9778b05d7da05ef8ad4e76a8874f753f33b528
Alan Ambrose <alanambrosedev@gmail.com>
2026-04-27 20:09:49 +0530

    feat: add Create, Edit, and Show components for brand management in admin dashboard

---
+++ src/components/admin/brand/Show.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Layout from "../../common/Layout";
+import Sidebar from "../../common/Sidebar";
+
+const Show = () => {
+  return (
+    <Layout>
+      <div className="container">
+        <div className="row">
+          <div className="d-flex justify-content-between mt-5 pb-3">
+            <h4 className="h4 pb-0 mb-0">Your Title</h4>
+          </div>
+          <Sidebar />
+          <div className="col-md-9">
+            <div className="card shadow">
+              <div className="card-body p-4"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default Show;
