6972ca2bfc86b5ffe1060c51f3d2c7eb7f812a8d
Alan Ambrose <alanambrosedev@gmail.com>
2026-05-04 22:37:44 +0530

    feat: implement brand deletion functionality with confirmation and error handling

--- src/components/admin/brand/Show.jsx
+++ src/components/admin/brand/Show.jsx
@@ -31,6 +31,32 @@ const Show = () => {
       console.error("Error in fetching brands: ", error);
     }
   };
+  const deleteBrand = async (id) => {
+    if (confirm("Are sure you want to delete this brand?")) {
+      try {
+        const res = await fetch(`${apiUrl}brands/${id}`, {
+          method: "DELETE",
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+            Authorization: `Bearer ${adminToken()}`,
+          },
+        });
+        const result = await res.json();
+        if (res.ok || result.success === 200) {
+          const newBrands = brands.filter((brand) => brand.id != id);
+
+          setBrands(newBrands);
+          toast.success(result.message);
+        } else {
+          toast.error(result.message || "Failed to delete category.");
+        }
+      } catch (error) {
+        console.log(error);
+        toast.error("Something went wrong. Please try again.");
+      }
+    }
+  };
   useEffect(() => {
     fetchBrands();
   }, []);
@@ -104,6 +130,7 @@ const Show = () => {
                                   fill="currentColor"
                                   className="bi bi-trash3"
                                   viewBox="0 0 16 16"
+                                  onClick={() => deleteBrand(brand.id)}
                                 >
                                   <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47M8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5" />
                                 </svg>
