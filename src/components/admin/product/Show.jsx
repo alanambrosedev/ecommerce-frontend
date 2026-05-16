91b0aa4cbcbdc3f7f0fb27a271e866e8b5b62a89
Alan Ambrose <alanambrosedev@gmail.com>
2026-05-16 22:57:08 +0530

    feat: implement product fetching and display in admin product show component

--- src/components/admin/product/Show.jsx
+++ src/components/admin/product/Show.jsx
@@ -1,7 +1,53 @@
-import React from "react";
+import React, { useEffect } from "react";
+import Sidebar from "../../common/Sidebar";
+import Layout from "../../common/Layout";
+import { Link } from "react-router-dom";
+import { adminToken, apiUrl } from "../../common/Http";
 
 const Show = () => {
-  return <div></div>;
+  const fetchProducts = async () => {
+    try {
+      const res = await fetch(`${apiUrl}products`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+          Authorization: `Bearer ${adminToken()}`,
+        },
+      });
+
+      if (!res.ok) {
+        throw new Error("Failed to fetch products");
+      }
+      const result = await res.json();
+      console.log(result);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+  return (
+    <Layout>
+      <div className="container">
+        <div className="row">
+          <div className="d-flex justify-content-between mt-5 pb-3">
+            <h4 className="h4 pb-0 mb-0">Products</h4>
+            <Link className="btn btn-primary" to="/admin/products/create">
+              Create
+            </Link>
+          </div>
+          <Sidebar />
+          <div className="col-md-9">
+            <div className="card shadow">
+              <div className="card-body p-4"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
 };
 
 export default Show;
