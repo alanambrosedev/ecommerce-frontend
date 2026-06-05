caa10c2084b00fbff912202d8d65afc7d63c5aba
Alan Ambrose <alanambrosedev@gmail.com>
2026-06-05 18:37:27 +0530

    feat: implement dynamic fetching of latest products in LatestProducts component

--- src/components/common/LatestProducts.jsx
+++ src/components/common/LatestProducts.jsx
@@ -1,68 +1,59 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ProductImg from "../../assets/images/mens/fivee.jpg";
+import { adminToken, apiUrl } from "../common/Http";
 
 const LatestProducts = () => {
+  const [latestProduct, setLatestProduct] = useState([]);
+  const fetchLatestProducts = async () => {
+    try {
+      const response = await fetch(`${apiUrl}latest-products`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+          Authorization: `Bearer ${adminToken()}`,
+        },
+      });
+      if (!response.ok) {
+        throw new Error("Failed to fetch products.");
+      }
+      const result = await response.json();
+      setLatestProduct(result.data);
+    } catch (error) {
+      console.error("Failed in fetching products.", error);
+    }
+  };
+  useEffect(() => {
+    fetchLatestProducts();
+  }, []);
   return (
     <section className="section-2 py-5">
       <div className="container">
         <h2>New Arrivals</h2>
         <div className="row mt-4">
-          <div className="col-md-3 col-6">
-            <div className="product card border-0">
-              <div className="card-img">
-                <img src={ProductImg} alt="" className="w-100" />
-              </div>
-              <div className="card-body pt-2">
-                <a href="">Violet Stripped Tshirt-Polo</a>
-                <div className="price">
-                  $50
-                  <span className="text-decoration-line-through">$90</span>
+          {latestProduct &&
+            latestProduct.map((product) => {
+              return (
+                <div className="col-md-3 col-6" key={product.id}>
+                  <div className="product card border-0">
+                    <div className="card-img">
+                      <img src={product.image_url} alt="" className="w-100" />
+                    </div>
+                    <div className="card-body pt-2">
+                      <a href="">{product.title}</a>
+                      <div className="price">
+                        {product.price} &nbsp;
+                        {product.compare_price && (
+                          <span className="text-decoration-line-through">
+                            {product.compare_price}
+                          </span>
+                        )}
+                      </div>
+                    </div>
+                  </div>
                 </div>
-              </div>
-            </div>
-          </div>
-          <div className="col-md-3 col-6">
-            <div className="product card border-0">
-              <div className="card-img">
-                <img src={ProductImg} alt="" className="w-100" />
-              </div>
-              <div className="card-body pt-2">
-                <a href="">Violet Stripped Tshirt-Polo</a>
-                <div className="price">
-                  $50
-                  <span className="text-decoration-line-through">$90</span>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="col-md-3 col-6">
-            <div className="product card border-0">
-              <div className="card-img">
-                <img src={ProductImg} alt="" className="w-100" />
-              </div>
-              <div className="card-body pt-2">
-                <a href="">Violet Stripped Tshirt-Polo</a>
-                <div className="price">
-                  $50
-                  <span className="text-decoration-line-through">$90</span>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="col-md-3 col-6">
-            <div className="product card border-0">
-              <div className="card-img">
-                <img src={ProductImg} alt="" className="w-100" />
-              </div>
-              <div className="card-body pt-2">
-                <a href="">Violet Stripped Tshirt-Polo</a>
-                <div className="price">
-                  $50
-                  <span className="text-decoration-line-through">$90</span>
-                </div>
-              </div>
-            </div>
-          </div>
+              );
+            })}
         </div>
       </div>
     </section>
