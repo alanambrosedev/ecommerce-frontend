0155fd1e3276a84065009c8a18a40e11df3b5d11
Alan Ambrose <alanambrosedev@gmail.com>
2026-06-07 18:54:19 +0530

    feat: implement category and brand filtering functionality in Shop component

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -7,6 +7,8 @@ const Shop = () => {
   const [categories, setCategories] = useState([]);
   const [brands, setBrands] = useState([]);
   const [products, setProducts] = useState([]);
+  const [catChecked, setCatChecked] = useState([]);
+  const [brandChecked, setBrandChecked] = useState([]);
   const fetchCategories = async () => {
     try {
       const response = await fetch(`${apiUrl}get-categories`, {
@@ -46,8 +48,20 @@ const Shop = () => {
     }
   };
   const fetchProducts = async () => {
+    let search = [];
+    let params = [];
+    if (catChecked.length > 0) {
+      search.push(["category", catChecked]);
+    }
+    if (brandChecked.length > 0) {
+      search.push(["brand", brandChecked]);
+    }
+    if (search.length > 0) {
+      params = new URLSearchParams(search);
+    }
+
     try {
-      const response = await fetch(`${apiUrl}get-products`, {
+      const response = await fetch(`${apiUrl}get-products?${params}`, {
         method: "GET",
         headers: {
           "Content-Type": "application/json",
@@ -64,101 +78,125 @@ const Shop = () => {
       console.error("Failed to fetch products.", error);
     }
   };
+
+  const handleCatSearch = (e) => {
+    const { checked, value } = e.target;
+    if (checked) {
+      setCatChecked((prev) => [...prev, value]);
+    } else {
+      setCatChecked((prev) => prev.filter((id) => id != value));
+    }
+  };
+
+  const handleBrandSearch = (e) => {
+    const { checked, value } = e.target;
+    if (checked) {
+      setBrandChecked((prev) => [...prev, value]);
+    } else {
+      setBrandChecked((prev) => prev.filter((id) => id != value));
+    }
+  };
   useEffect(() => {
     fetchCategories();
     fetchBrands();
     fetchProducts();
-  }, []);
+  }, [catChecked, brandChecked]);
   return (
-    <>
-      <Layout>
-        <div className="container py-4">
-          <nav aria-label="breadcrumb">
-            <ol class="breadcrumb">
-              <li class="breadcrumb-item">
-                <a href="/">Home</a>
-              </li>
-              <li class="breadcrumb-item active" aria-current="page">
-                Shop
-              </li>
-            </ol>
-          </nav>
-          <div className="row">
-            <div className="col-md-3">
-              <div className="card shadow border-0 mb-3">
-                <div className="card p-4">
-                  <h3 className="mb-3">Categories</h3>
-                  <ul>
-                    {categories &&
-                      categories.map((category) => {
-                        return (
-                          <li className="mb-2" key={category.id}>
-                            <input type="checkbox" />
-                            <label htmlFor="" className="ps-2">
-                              {category.name}
-                            </label>
-                          </li>
-                        );
-                      })}
-                  </ul>
-                </div>
-              </div>
-              <div className="card shadow border-0 mb-3">
-                <div className="card p-4">
-                  <h3 className="mb-3">Brands</h3>
-                  <ul>
-                    {brands &&
-                      brands.map((brand) => {
-                        return (
-                          <li className="mb-2" key={brand.id}>
-                            <input type="checkbox" />
-                            <label htmlFor="" className="ps-2">
-                              {brand.name}
-                            </label>
-                          </li>
-                        );
-                      })}
-                  </ul>
-                </div>
+    <Layout>
+      <div className="container py-4">
+        <nav aria-label="breadcrumb">
+          <ol className="breadcrumb">
+            <li className="breadcrumb-item">
+              <a href="/">Home</a>
+            </li>
+            <li className="breadcrumb-item active" aria-current="page">
+              Shop
+            </li>
+          </ol>
+        </nav>
+        <div className="row">
+          <div className="col-md-3">
+            <div className="card shadow border-0 mb-3">
+              <div className="card p-4">
+                <h3 className="mb-3">Categories</h3>
+                <ul>
+                  {categories &&
+                    categories.map((category) => {
+                      return (
+                        <li className="mb-2" key={category.id}>
+                          <input
+                            type="checkbox"
+                            value={category.id}
+                            onClick={handleCatSearch}
+                          />
+                          <label htmlFor="" className="ps-2">
+                            {category.name}
+                          </label>
+                        </li>
+                      );
+                    })}
+                </ul>
               </div>
             </div>
-            <div className="col-md-9">
-              <div className="row">
-                {products &&
-                  products.map((product) => {
-                    return (
-                      <div className="col-md-4 col-6" key={product.id}>
-                        <div className="product card border-0">
-                          <div className="card-img">
-                            <Link to="/product">
-                              <img
-                                src={product.image_url}
-                                alt=""
-                                className="w-100"
-                              />
-                            </Link>
-                          </div>
-                          <div className="card-body pt-2">
-                            <Link to="/product">{product.title}</Link>
-                            <div className="price">
-                              ${product.price} &nbsp;
-                              {product.compare_price && (
-                                <span className="text-decoration-line-through">
-                                  ${product.compare_price}
-                                </span>
-                              )}
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    );
-                  })}
+            <div className="card shadow border-0 mb-3">
+              <div className="card p-4">
+                <h3 className="mb-3">Brands</h3>
+                <ul>
+                  {brands &&
+                    brands.map((brand) => {
+                      return (
+                        <li className="mb-2" key={brand.id}>
+                          <input
+                            type="checkbox"
+                            value={brand.id}
+                            onClick={handleBrandSearch}
+                          />
+                          <label htmlFor="" className="ps-2">
+                            {brand.name}
+                          </label>
+                        </li>
+                      );
+                    })}
+                </ul>
               </div>
             </div>
           </div>
+          <div className="col-md-9">
+            <div className="row">
+              {products &&
+                products.map((product) => {
+                  return (
+                    <div className="col-md-4 col-6" key={product.id}>
+                      <div className="product card border-0">
+                        <div className="card-img">
+                          <Link to="/product">
+                            <img
+                              src={product.image_url}
+                              alt=""
+                              className="w-100"
+                            />
+                          </Link>
+                        </div>
+                        <div className="card-body pt-2">
+                          <Link to="/product">{product.title}</Link>
+                          <div className="price">
+                            ${product.price} &nbsp;
+                            {product.compare_price && (
+                              <span className="text-decoration-line-through">
+                                ${product.compare_price}
+                              </span>
+                            )}
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  );
+                })}
+            </div>
+          </div>
         </div>
-      </Layout>
-    </>
+      </div>
+    </Layout>
   );
 };
 
