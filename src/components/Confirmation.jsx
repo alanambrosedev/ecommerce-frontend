ebeced0ea05dfac893fbe3158616ad0311161b59
Alan Ambrose <alanambrosedev@gmail.com>
2026-07-15 21:21:48 +0530

    feat: enhance Confirmation component with order summary and user details

--- src/components/Confirmation.jsx
+++ src/components/Confirmation.jsx
@@ -5,8 +5,97 @@ import Layout from "./common/Layout";
 const Confirmation = () => {
   return (
     <Layout>
-      <div className="container">
-        <div className="row"></div>
+      <div className="container py-5">
+        <div className="row">
+          <h1 className="text-center fw-bold text-success">Thank You!</h1>
+          <p className="text-muted text-center">
+            Your order is successfully placed.
+          </p>
+        </div>
+        <div className="card shadow">
+          <div className="card-body">
+            <h3 className="fw-bold">Order Summary</h3>
+            <hr />
+            <div className="row">
+              <div className="col-6">
+                <p>
+                  <strong>Order ID: </strong>
+                  #2323
+                </p>
+                <p>
+                  <strong>Date: </strong>
+                  17 March 2026
+                </p>
+                <p>
+                  <strong>Status: </strong>
+                  <span className="badge bg-warning">Pending</span>
+                </p>
+                <p>
+                  <strong>Payment Method: </strong>COD
+                </p>
+              </div>
+              <div className="col-6">
+                <p>
+                  <strong>Customer: </strong>Alan
+                </p>
+                <p>
+                  <strong>Address: </strong>Lorem ipsum
+                </p>
+                <p>
+                  <strong>Contact:</strong>9845345745
+                </p>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-12">
+                <table className="table-stripped table-bordered table">
+                  <thead className="table-light">
+                    <tr>
+                      <th>Item</th>
+                      <th>Quantity</th>
+                      <th>Price</th>
+                      <th>Total</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>Product 1</td>
+                      <td>2</td>
+                      <td>$100</td>
+                      <td>$200</td>
+                    </tr>
+                  </tbody>
+                  <tfoot>
+                    <tr>
+                      <td className="text-end fw-bold" colSpan={3}>
+                        Sub Total
+                      </td>
+                      <td>$1000</td>
+                    </tr>
+                    <tr>
+                      <td className="text-end fw-bold" colSpan={3}>
+                        Shipping
+                      </td>
+                      <td>$12</td>
+                    </tr>
+                    <tr>
+                      <td className="text-end fw-bold" colSpan={3}>
+                        Grand Total
+                      </td>
+                      <td>$1000</td>
+                    </tr>
+                  </tfoot>
+                </table>
+              </div>
+            </div>
+            <div className="text-center">
+              <button className="btn btn-primary">Order Details</button>
+              <button className="btn btn-outline-secondary ms-2">
+                Continue Shopping
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     </Layout>
   );
