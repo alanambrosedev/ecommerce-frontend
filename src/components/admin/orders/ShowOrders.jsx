6dfb05d853d4c25db1e392073b6d1e896cf66bdb
Alan Ambrose <alanambrosedev@gmail.com>
2026-07-25 22:21:49 +0530

    feat: enhance ShowOrders component with loading state and order status display

--- src/components/admin/orders/ShowOrders.jsx
+++ src/components/admin/orders/ShowOrders.jsx
@@ -4,6 +4,8 @@ import Layout from "../../common/Layout";
 import { Link } from "react-router-dom";
 import { adminToken, apiUrl } from "../../common/Http";
 import { toast } from "react-toastify";
+import Loader from "../../common/Loader";
+import NoState from "../../common/NoState";
 
 const ShowOrders = () => {
   const [orders, setOrders] = useState([]);
@@ -24,11 +26,29 @@ const ShowOrders = () => {
       }
       const result = await res.json();
       setLoading(false);
+      console.log(result.data);
 
       setOrders(result.data);
     } catch (error) {
       console.log(error);
       toast.error("Something went wrong.");
+    }
+  };
+
+  const getStatus = (status) => {
+    switch (status) {
+      case "pending":
+        return "bg-warning";
+      case "delivered":
+      case "paid":
+        return "bg-success";
+      case "shipped":
+        return "bg-info";
+      case "cancelled":
+      case "not paid":
+        return "bg-danger";
+      default:
+        return "bg-secondary";
     }
   };
   useEffect(() => {
@@ -44,7 +64,57 @@ const ShowOrders = () => {
           <Sidebar />
           <div className="col-md-9">
             <div className="card shadow">
-              <div className="card-body p-4"></div>
+              <div className="card-body p-4">
+                {loading == true && <Loader />}
+                {loading == false && orders.length == 0 && (
+                  <NoState text="Orders not found." />
+                )}
+                {loading == false && orders.length > 0 && (
+                  <table className="table table-hover">
+                    <thead>
+                      <tr>
+                        <th>ID</th>
+                        <th>Customer</th>
+                        <th>Email</th>
+                        <th>Amount</th>
+                        <th>Date</th>
+                        <th>Payment</th>
+                        <th>Status</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {orders &&
+                        orders.map((order) => {
+                          return (
+                            <tr key={order.id}>
+                              <td className="text-primary">{order.id}</td>
+                              <td>{order.name}</td>
+                              <td>{order.email}</td>
+                              <td>
+                                ${parseFloat(order.grand_total).toFixed(2)}
+                              </td>
+                              <td>{order.created_at}</td>
+                              <td>
+                                <span
+                                  className={`badge ${getStatus(order?.payment_status)}`}
+                                >
+                                  {order?.payment_status}
+                                </span>
+                              </td>
+                              <td>
+                                <span
+                                  className={`badge ${getStatus(order?.status)}`}
+                                >
+                                  {order?.status}
+                                </span>
+                              </td>
+                            </tr>
+                          );
+                        })}
+                    </tbody>
+                  </table>
+                )}
+              </div>
             </div>
           </div>
         </div>
